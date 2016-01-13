1bec3b197c7d62aef842fd845028449c8af26c91
neyosuraj <neyosuraj@gmail.com>
2016-01-13 13:40:18 +0545

    creating customer details

---
+++ Jewellery management system/CustomerDetails.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace Jewellery_management_system
+{
+    partial class CustomerDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbo_customerId = new System.Windows.Forms.ComboBox();
+            this.lbl_CustomerId = new System.Windows.Forms.Label();
+            this.lbl_newCustomerId = new System.Windows.Forms.Label();
+            this.txt_newCustomerID = new System.Windows.Forms.TextBox();
+            this.lbl_title = new System.Windows.Forms.Label();
+            this.cbo_title = new System.Windows.Forms.ComboBox();
+            this.cbo_name = new System.Windows.Forms.ComboBox();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_spouse = new System.Windows.Forms.TextBox();
+            this.temp_adress = new System.Windows.Forms.Label();
+            this.txt_temp_adress = new System.Windows.Forms.TextBox();
+            this.txt_city = new System.Windows.Forms.TextBox();
+            this.lbl_city = new System.Windows.Forms.Label();
+            this.lbl_birthdate = new System.Windows.Forms.Label();
+            this.dtp_birthdate = new System.Windows.Forms.DateTimePicker();
+            this.dtp_anniversary = new System.Windows.Forms.DateTimePicker();
+            this.lbl_anniversary = new System.Windows.Forms.Label();
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.lbl_phone = new System.Windows.Forms.Label();
+            this.txt_mobile = new System.Windows.Forms.TextBox();
+            this.lbl_mobile = new System.Windows.Forms.Label();
+            this.txt_fax = new System.Windows.Forms.TextBox();
+            this.lbl_fax = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cbo_customerId
+            // 
+            this.cbo_customerId.FormattingEnabled = true;
+            this.cbo_customerId.Location = new System.Drawing.Point(151, 33);
+            this.cbo_customerId.Name = "cbo_customerId";
+            this.cbo_customerId.Size = new System.Drawing.Size(121, 21);
+            this.cbo_customerId.TabIndex = 0;
+            // 
+            // lbl_CustomerId
+            // 
+            this.lbl_CustomerId.AutoSize = true;
+            this.lbl_CustomerId.Location = new System.Drawing.Point(86, 37);
+            this.lbl_CustomerId.Name = "lbl_CustomerId";
+            this.lbl_CustomerId.Size = new System.Drawing.Size(59, 13);
+            this.lbl_CustomerId.TabIndex = 1;
+            this.lbl_CustomerId.Text = "customerId";
+            // 
+            // lbl_newCustomerId
+            // 
+            this.lbl_newCustomerId.AutoSize = true;
+            this.lbl_newCustomerId.Location = new System.Drawing.Point(334, 38);
+            this.lbl_newCustomerId.Name = "lbl_newCustomerId";
+            this.lbl_newCustomerId.Size = new System.Drawing.Size(88, 13);
+            this.lbl_newCustomerId.TabIndex = 2;
+            this.lbl_newCustomerId.Text = "New Customer Id";
+            // 
+            // txt_newCustomerID
+            // 
+            this.txt_newCustomerID.Location = new System.Drawing.Point(437, 35);
+            this.txt_newCustomerID.Name = "txt_newCustomerID";
+            this.txt_newCustomerID.Size = new System.Drawing.Size(100, 20);
+            this.txt_newCustomerID.TabIndex = 3;
+            // 
+            // lbl_title
+            // 
+            this.lbl_title.AutoSize = true;
+            this.lbl_title.Location = new System.Drawing.Point(118, 69);
+            this.lbl_title.Name = "lbl_title";
+            this.lbl_title.Size = new System.Drawing.Size(27, 13);
+            this.lbl_title.TabIndex = 4;
+            this.lbl_title.Text = "Title";
+            // 
+            // cbo_title
+            // 
+            this.cbo_title.FormattingEnabled = true;
+            this.cbo_title.Items.AddRange(new object[] {
+            "Mr",
+            "Mrs",
+            "Dr",
+            "Stu"});
+            this.cbo_title.Location = new System.Drawing.Point(151, 66);
+            this.cbo_title.Name = "cbo_title";
+            this.cbo_title.Size = new System.Drawing.Size(121, 21);
+            this.cbo_title.TabIndex = 0;
+            // 
+            // cbo_name
+            // 
+            this.cbo_name.FormattingEnabled = true;
+            this.cbo_name.Location = new System.Drawing.Point(437, 69);
+            this.cbo_name.Name = "cbo_name";
+            this.cbo_name.Size = new System.Drawing.Size(400, 21);
+            this.cbo_name.TabIndex = 0;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(368, 72);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(35, 13);
+            this.lbl_name.TabIndex = 4;
+            this.lbl_name.Text = "Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(102, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Spouse";
+            // 
+            // txt_spouse
+            // 
+            this.txt_spouse.Location = new System.Drawing.Point(151, 132);
+            this.txt_spouse.Name = "txt_spouse";
+            this.txt_spouse.Size = new System.Drawing.Size(200, 20);
+            this.txt_spouse.TabIndex = 5;
+            // 
+            // temp_adress
+            // 
+            this.temp_adress.AutoSize = true;
+            this.temp_adress.Location = new System.Drawing.Point(53, 167);
+            this.temp_adress.Name = "temp_adress";
+            this.temp_adress.Size = new System.Drawing.Size(92, 13);
+            this.temp_adress.TabIndex = 4;
+            this.temp_adress.Text = "Temporary Adress";
+            // 
+            // txt_temp_adress
+            // 
+            this.txt_temp_adress.Location = new System.Drawing.Point(151, 167);
+            this.txt_temp_adress.Name = "txt_temp_adress";
+            this.txt_temp_adress.Size = new System.Drawing.Size(200, 20);
+            this.txt_temp_adress.TabIndex = 5;
+            // 
+            // txt_city
+            // 
+            this.txt_city.Location = new System.Drawing.Point(151, 210);
+            this.txt_city.Name = "txt_city";
+            this.txt_city.Size = new System.Drawing.Size(200, 20);
+            this.txt_city.TabIndex = 7;
+            // 
+            // lbl_city
+            // 
+            this.lbl_city.AutoSize = true;
+            this.lbl_city.Location = new System.Drawing.Point(53, 210);
+            this.lbl_city.Name = "lbl_city";
+            this.lbl_city.Size = new System.Drawing.Size(92, 13);
+            this.lbl_city.TabIndex = 6;
+            this.lbl_city.Text = "Temporary Adress";
+            // 
+            // lbl_birthdate
+            // 
+            this.lbl_birthdate.AutoSize = true;
+            this.lbl_birthdate.Location = new System.Drawing.Point(381, 131);
+            this.lbl_birthdate.Name = "lbl_birthdate";
+            this.lbl_birthdate.Size = new System.Drawing.Size(52, 13);
+            this.lbl_birthdate.TabIndex = 8;
+            this.lbl_birthdate.Text = "Birth date";
+            // 
+            // dtp_birthdate
+            // 
+            this.dtp_birthdate.Location = new System.Drawing.Point(469, 124);
+            this.dtp_birthdate.Name = "dtp_birthdate";
+            this.dtp_birthdate.Size = new System.Drawing.Size(200, 20);
+            this.dtp_birthdate.TabIndex = 9;
+            // 
+            // dtp_anniversary
+            // 
+            this.dtp_anniversary.Location = new System.Drawing.Point(469, 167);
+            this.dtp_anniversary.Name = "dtp_anniversary";
+            this.dtp_anniversary.Size = new System.Drawing.Size(200, 20);
+            this.dtp_anniversary.TabIndex = 11;
+            // 
+            // lbl_anniversary
+            // 
+            this.lbl_anniversary.AutoSize = true;
+            this.lbl_anniversary.Location = new System.Drawing.Point(371, 167);
+            this.lbl_anniversary.Name = "lbl_anniversary";
+            this.lbl_anniversary.Size = new System.Drawing.Size(62, 13);
+            this.lbl_anniversary.TabIndex = 10;
+            this.lbl_anniversary.Text = "Anniversary";
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(469, 210);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(200, 20);
+            this.txt_phone.TabIndex = 13;
+            // 
+            // lbl_phone
+            // 
+            this.lbl_phone.AutoSize = true;
+            this.lbl_phone.Location = new System.Drawing.Point(396, 210);
+            this.lbl_phone.Name = "lbl_phone";
+            this.lbl_phone.Size = new System.Drawing.Size(37, 13);
+            this.lbl_phone.TabIndex = 12;
+            this.lbl_phone.Text = "phone";
+            // 
+            // txt_mobile
+            // 
+            this.txt_mobile.Location = new System.Drawing.Point(469, 238);
+            this.txt_mobile.Name = "txt_mobile";
+            this.txt_mobile.Size = new System.Drawing.Size(200, 20);
+            this.txt_mobile.TabIndex = 15;
+            // 
+            // lbl_mobile
+            // 
+            this.lbl_mobile.AutoSize = true;
+            this.lbl_mobile.Location = new System.Drawing.Point(396, 245);
+            this.lbl_mobile.Name = "lbl_mobile";
+            this.lbl_mobile.Size = new System.Drawing.Size(37, 13);
+            this.lbl_mobile.TabIndex = 14;
+            this.lbl_mobile.Text = "mobile";
+            // 
+            // txt_fax
+            // 
+            this.txt_fax.Location = new System.Drawing.Point(469, 273);
+            this.txt_fax.Name = "txt_fax";
+            this.txt_fax.Size = new System.Drawing.Size(200, 20);
+            this.txt_fax.TabIndex = 17;
+            // 
+            // lbl_fax
+            // 
+            this.lbl_fax.AutoSize = true;
+            this.lbl_fax.Location = new System.Drawing.Point(409, 273);
+            this.lbl_fax.Name = "lbl_fax";
+            this.lbl_fax.Size = new System.Drawing.Size(24, 13);
+            this.lbl_fax.TabIndex = 16;
+            this.lbl_fax.Text = "Fax";
+            this.lbl_fax.Click += new System.EventHandler(this.lbl_fax_Click);
+            // 
+            // CustomerDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 488);
+            this.Controls.Add(this.txt_fax);
+            this.Controls.Add(this.lbl_fax);
+            this.Controls.Add(this.txt_mobile);
+            this.Controls.Add(this.lbl_mobile);
+            this.Controls.Add(this.txt_phone);
+            this.Controls.Add(this.lbl_phone);
+            this.Controls.Add(this.dtp_anniversary);
+            this.Controls.Add(this.lbl_anniversary);
+            this.Controls.Add(this.dtp_birthdate);
+            this.Controls.Add(this.lbl_birthdate);
+            this.Controls.Add(this.txt_city);
+            this.Controls.Add(this.lbl_city);
+            this.Controls.Add(this.txt_temp_adress);
+            this.Controls.Add(this.txt_spouse);
+            this.Controls.Add(this.temp_adress);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_title);
+            this.Controls.Add(this.txt_newCustomerID);
+            this.Controls.Add(this.lbl_newCustomerId);
+            this.Controls.Add(this.lbl_CustomerId);
+            this.Controls.Add(this.cbo_name);
+            this.Controls.Add(this.cbo_title);
+            this.Controls.Add(this.cbo_customerId);
+            this.Name = "CustomerDetails";
+            this.Text = "CustomerDetails";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbo_customerId;
+        private System.Windows.Forms.Label lbl_CustomerId;
+        private System.Windows.Forms.Label lbl_newCustomerId;
+        private System.Windows.Forms.TextBox txt_newCustomerID;
+        private System.Windows.Forms.Label lbl_title;
+        private System.Windows.Forms.ComboBox cbo_title;
+        private System.Windows.Forms.ComboBox cbo_name;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_spouse;
+        private System.Windows.Forms.Label temp_adress;
+        private System.Windows.Forms.TextBox txt_temp_adress;
+        private System.Windows.Forms.TextBox txt_city;
+        private System.Windows.Forms.Label lbl_city;
+        private System.Windows.Forms.Label lbl_birthdate;
+        private System.Windows.Forms.DateTimePicker dtp_birthdate;
+        private System.Windows.Forms.DateTimePicker dtp_anniversary;
+        private System.Windows.Forms.Label lbl_anniversary;
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.Label lbl_phone;
+        private System.Windows.Forms.TextBox txt_mobile;
+        private System.Windows.Forms.Label lbl_mobile;
+        private System.Windows.Forms.TextBox txt_fax;
+        private System.Windows.Forms.Label lbl_fax;
+    }
+}
---
+++ Jewellery management system/CustomerDetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jewellery_management_system
+{
+    public partial class CustomerDetails : Form
+    {
+        public CustomerDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void lbl_fax_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
